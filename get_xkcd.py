c7bfacfa9de0086f12af10d961c127889603e3fe
Slava Petrov <slavik--petrov@yandex.ru>
2017-03-03 15:41:58 +0400

    Download image

--- get_xkcd.py
+++ get_xkcd.py
@@ -13,8 +13,22 @@ while not url.endswith('#'):
 	res = requests.get(url)
 	res.raise_for_status()
 	soup = bs4.BeautifulSoup(res.text)
-	#TODO: find the url of image
-	#TODO: download the image
+	
+	comicElem = soup.select('#comic img')
+	if comicElem == []:
+		print('Could not find comic image.')
+	else:
+		try:
+			comicUrl = 'http:' + comicElem[0].get('src')
+			print('Downloading image %s...' % (comicUrl))
+			res = requests.get(comicUrl)
+			res.raise_for_status()
+		except requests.exceptions.MissingSchema:
+			# skip this comic
+			prevLink = soup.select('a[rel="prev"]')[0]
+			url = 'http://xkcd.com' + prevLink
+			continue
+	
 	#TODO: save image to ./xkcd
 	#TODO: get the Prev button's url
 print('Done.')
