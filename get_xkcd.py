61cb9eed97e5c3b64627631b5bea60b853afbe28
Slava Petrov <slavik--petrov@yandex.ru>
2017-03-03 15:36:37 +0400

    Download page

--- get_xkcd.py
+++ get_xkcd.py
@@ -9,6 +9,10 @@ url = 'http://xkcd.com'
 os.makedirs('xkcd', exist_ok=True)
 while not url.endswith('#'):
 	#TODO: download the page
+	print('Downloading page %s...' % url)
+	res = requests.get(url)
+	res.raise_for_status()
+	soup = bs4.BeautifulSoup(res.text)
 	#TODO: find the url of image
 	#TODO: download the image
 	#TODO: save image to ./xkcd
